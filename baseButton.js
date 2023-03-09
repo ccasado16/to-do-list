0c9c73a9bfb83605c577b8e22fb381cfd84b88f2
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-03-09 15:09:26 -0500

    Add class to baseButton

--- baseButton.js
+++ baseButton.js
@@ -1,6 +1,7 @@
 function baseButton(text, event) {
   let button = document.createElement("button");
   button.innerText = text;
+  button.className = "btn";
   button.addEventListener("click", event);
 
   return button;
