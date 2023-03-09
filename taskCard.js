1602763872f23106029f06d62640cbe1e802cb19
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-03-09 14:25:06 -0500

    Add Task to DOM element and render with buttons

--- taskCard.js
+++ taskCard.js
@@ -2,15 +2,25 @@ export function TaskCard(task, completeTask, removeTask) {
   let li = document.createElement("li");
   li.innerText = `${task.name} ${task.completed}`;
 
+  li.appendChild(btnComplete(completeTask, task));
+
+  li.appendChild(btnDelete(removeTask, task));
+
+  return li;
+}
+
+function btnComplete(completeTask, task) {
   let btnComplete = document.createElement("button");
   btnComplete.innerText = "Complete";
   btnComplete.addEventListener("click", () => completeTask(task));
-  li.appendChild(btnComplete);
 
+  return btnComplete;
+}
+
+function btnDelete(removeTask, task) {
   let btnDelete = document.createElement("button");
   btnDelete.innerText = "Remove";
   btnDelete.addEventListener("click", () => removeTask(task));
-  li.appendChild(btnDelete);
 
-  return li;
+  return btnDelete;
 }
