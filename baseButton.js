2b0dd22ca5ec2bf13073844c2e4c905cc6960293
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-03-09 19:23:56 -0500

    Add styles to actions buttons

--- baseButton.js
+++ baseButton.js
@@ -1,7 +1,7 @@
-function baseButton(text, event) {
+function baseButton(icon, event) {
   let button = document.createElement("button");
-  button.innerText = text;
   button.classList.add("btn");
+  button.innerHTML = icon || "button";
   button.addEventListener("click", event);
 
   return button;
